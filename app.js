c6180cd8e50e99689a3b4ddca989d6079f51103d
taisan0816 <guruyo7835@gmail.com>
2025-01-30 20:38:26 +0900

    プロジェクトの環境構築

---
+++ app.js
@@ -0,0 +1,21 @@
+const PORT = process.env.PORT;
+const path = require("path");
+const express = require("express");
+const favicon = require("serve-favicon");
+const app = express();
+
+//テンプレートエンジンのejsを使うときの決まり文句
+app.set("view engine", "ejs");
+
+// Static resource routing
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public",express.static(path.join(__dirname, "/public")));
+
+// Dynamic resource rooting
+app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, ()=> {
+    console.log(`Application listening at ${PORT}`);
+});
+
+
