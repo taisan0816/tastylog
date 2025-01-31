3d462cc47e4270c1b84a42f90a5a9a5d680914b5
taisan0816 <guruyo7835@gmail.com>
2025-01-31 13:10:18 +0900

    ログ出力の実装

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const PORT = process.env.PORT;
 const path = require("path");
+const logger = require("./lib/log/logger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -14,11 +17,18 @@ app.disable("x-powered-by");
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public",express.static(path.join(__dirname, "/public")));
 
+//Set access log
+//静的コンテンツのログは不要なので、そのあとに書く
+app.use(accesslogger());
+
 // Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
 
+// Set application log.
+app.use(applicationlogger());
+
 app.listen(PORT, ()=> {
-    console.log(`Application listening at ${PORT}`);
+    logger.application.info(`Application listening at ${PORT}`);
 });
 
 
