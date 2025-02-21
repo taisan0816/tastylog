ce8f00d5b9337d3c19c4d3826b365b3eafba828f
taisan0816 <guruyo7835@gmail.com>
2025-02-21 23:15:24 +0900

    feat(feature/6):二十送信防止のためにボタンの仕様変更

--- public/javascripts/regist.js
+++ public/javascripts/regist.js
@@ -4,6 +4,8 @@ let btnSubmit_onclick = function(event) {
     $form.attr("method",  $submit.data("method"));
     $form.attr("action", $submit.data("action"));
     $form.submit();
+    $submit.off().prop("disabled", true);
+    $form.on("submit", false);
 }
 
 let document_onready = function(event) {
