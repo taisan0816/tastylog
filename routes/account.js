56eaee63730f8c9bb07bdbc2d0c820d154b00aaa
taisan0816 <guruyo7835@gmail.com>
2025-02-23 20:31:21 +0900

    feat(feature/7):ログイン画面作成

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/login", (req, res, next) => {
+    res.render("./account/login.ejs");
+});
+
 router.use("/reviews", require("./account.reviews.js"));
 
 module.exports = router;
